9b1c307963d1ac6f47a839dd630869f0bcd7a49c
Petr Moravek <petr.moravek@t-mobile.cz>
2013-04-07 06:02:58 +0200

    testSVC finished

--- src/com/example/libAPI/TestActivity.java
+++ src/com/example/libAPI/TestActivity.java
@@ -1,15 +1,226 @@
 package com.example.libAPI;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.SignatureException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
-public class TestActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.aidl.ILogService;
+import com.example.aidl.IMqttCb;
+import com.example.aidl.Message;
+
+public class TestActivity extends Activity implements OnClickListener {
+	private static final String TAG = "Example";
+
+	ILogService logService;
+	LogConnection conn;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// Request bind to the service
+		conn = new LogConnection();
+		Intent intent = new Intent("com.example.svc.Logger");
+		intent.putExtra("version", "1.0");
+		boolean bindSvc = bindService(intent, conn, Context.BIND_AUTO_CREATE);
+		Log.d(TAG, "bindService = " + bindSvc);
+
+		Button clr = (Button) findViewById(R.id.button1);
+		clr.setOnClickListener(this);
+
+		String packageName = this.getPackageName();
+		testCert(packageName);
+	}
+
+	public void testCert(String packageName) {
+		PackageManager pm = this.getPackageManager();
+
+		int flags = PackageManager.GET_SIGNATURES;
+
+		PackageInfo packageInfo = null;
+
+		try {
+			packageInfo = pm.getPackageInfo(packageName, flags);
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		Signature[] signatures = packageInfo.signatures;
+
+		Log.d(TAG, "Number of signatures: " + signatures.length);
+
+		for (Signature sign : signatures) {
+			byte[] cert = sign.toByteArray();
+
+			InputStream input = new ByteArrayInputStream(cert);
+
+			CertificateFactory cf = null;
+			try {
+				cf = CertificateFactory.getInstance("X509");
+
+			} catch (CertificateException e) {
+				e.printStackTrace();
+				return;
+			}
+
+			X509Certificate c = null;
+
+			try {
+				c = (X509Certificate) cf.generateCertificate(input);
+			} catch (CertificateException e) {
+				e.printStackTrace();
+				return;
+			}
+
+			Log.d(TAG, "getSigAlgName: " + c.getSigAlgName());
+			Log.d(TAG, "getIssuerDN: " + c.getIssuerDN());
+			Log.d(TAG, "getSubjectDN: " + c.getSubjectDN());
+
+			PublicKey key;
+			// if self signed then it works
+			key = c.getPublicKey();
+			try {
+				c.verify(key);
+			} catch (InvalidKeyException e) {
+				e.printStackTrace();
+				return;
+			} catch (CertificateException e) {
+				e.printStackTrace();
+				return;
+			} catch (NoSuchAlgorithmException e) {
+				e.printStackTrace();
+				return;
+			} catch (NoSuchProviderException e) {
+				e.printStackTrace();
+				return;
+			} catch (SignatureException e) {
+				e.printStackTrace();
+				return;
+			}
+			Log.d(TAG, "Signature verified");
+
+			try {
+				MessageDigest md = MessageDigest.getInstance("SHA1");
+				byte[] publicKey = md.digest(c.getPublicKey().getEncoded());
+
+				StringBuffer hexString = new StringBuffer();
+				for (int i = 0; i < publicKey.length; i++) {
+					String appendString = Integer
+							.toHexString(0xFF & publicKey[i]);
+					if (appendString.length() == 1)
+						hexString.append("0");
+					hexString.append(appendString);
+				}
+
+				Log.d(TAG, "Cert hash: " + hexString.toString());
+
+			} catch (NoSuchAlgorithmException e1) {
+				e1.printStackTrace();
+				return;
+			}
+		}
+	}
+
+	public void testSvc() {
+		try {
+			Log.d(TAG, "testSVC");
+			logService.log_d("LogClient", "Hello from onClick()");
+			Parcel pcl = Parcel.obtain();
+			Message msg = new Message(pcl);
+			msg.setTag("LogClient");
+			msg.setText("Send msg");
+			logService.pub("OOOOO náš pán");
+			logService.log(msg);
+			// register callback
+			logService.regCallBack(cb);
+			// logService.pub("Toto je loto ščýžýž+šáíáéřěíščřčš");
+			String ret = logService.syncSendRecv("MyW/BackForth",
+					"Send To Backend");
+
+			debugToast("SynchroMsg: " + ret);
+			pcl.recycle();
+		} catch (RemoteException e) { //
+			Log.e(TAG, "onClick failed", e);
+		}
+	}
+
+	class LogConnection implements ServiceConnection {
+
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			logService = ILogService.Stub.asInterface(service);
+			debugToast("LogConnection.onServiceConnected");
+		}
+
+		public void onServiceDisconnected(ComponentName name) { //
+			logService = null;
+			debugToast("LogConnection.onServiceDisconnected");
+		}
+	}
+
+	private void msgImpl(String topic, String result) {
+		debugToast("topic: " + topic + "\nvalue: " + result);
+	}
+
+	private void msgAlert(String topic, String result) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(topic);
+		builder.setMessage(result);
+		builder.setPositiveButton("OK", null);
+		builder.show();
+	}
+
+	private final IMqttCb.Stub cb = new IMqttCb.Stub() {
+
+		@Override
+		public void msg(final String topic, final String result)
+				throws RemoteException {
+			runOnUiThread(new Runnable() {
+				public void run() {
+					msgImpl(topic, result);
+				}
+			});
+		}
+
+	};
+
+	public void debugToast(String what) {
+		Log.d(TAG, what);
+		Toast toast = Toast.makeText(this, what, Toast.LENGTH_SHORT);
+		toast.show();
+	}
+
+	@Override
+	public void onClick(View v) {
+		Log.d(TAG, "onClick: " + v.toString());
+		testSvc();
+	}
 }
---
+++ src/com/example/spServices/spService.java
@@ -0,0 +1,132 @@
+package com.example.spServices;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.example.aidl.ILogService;
+import com.example.aidl.IMqttCb;
+import com.example.aidl.Message;
+import com.example.libAPI.R;
+import com.example.libAPI.TestActivity;
+
+public class spService extends Service {
+
+	private static final int MY_NOTIF_ID = 0;
+	private static final int PI_REQ_CODE = 1;
+	protected static final String TAG = "spService";
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		final String version = intent.getExtras().getString("version");
+
+		Log.d(TAG, "onBind: version requested: " + version);
+
+		return new ILogService.Stub() {
+
+			IMqttCb savedCb;
+
+			public void log_d(String tag, String message)
+					throws RemoteException {
+				Log.d(TAG, "tag: " + tag + " / msg: " + message + " version: "
+						+ version);
+			}
+
+			public void log(Message msg) throws RemoteException {
+				Log.d(TAG, "tag: " + msg.getTag() + " / msg:" + msg.getText());
+			}
+
+			public void regCallBack(IMqttCb cb) {
+				Log.d(TAG, "regCallBack");
+				savedCb = cb;
+			}
+
+			@Override
+			public void pub(String txt) throws RemoteException {
+				Log.d(TAG, "pub: " + txt);
+				Runnable busyLoop = new Runnable() {
+					public void run() {
+						int count = 1;
+						while (count < 5) {
+							count++;
+							try {
+								Thread.sleep(1000);
+							} catch (Exception ex) {
+								;
+							}
+							sendNotification("progress + " + count,
+									"busyLoop - " + count);
+						}
+						sendNotification("done + " + count, "end - " + count);
+						if (savedCb != null) {
+							try {
+								savedCb.msg("done work", "OK!");
+							} catch (RemoteException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+				};
+				performOnBackgroundThread(busyLoop);
+			}
+
+			@Override
+			public String syncSendRecv(String topic, String txt)
+					throws RemoteException {
+				Log.d(TAG, "syncSendRecv");
+
+				return "nothing";
+			}
+		};
+	}
+
+	public static Thread performOnBackgroundThread(final Runnable runnable) {
+		final Thread t = new Thread() {
+			@Override
+			public void run() {
+				try {
+					runnable.run();
+				} finally {
+
+				}
+			}
+		};
+		t.start();
+		return t;
+	}
+
+	public void sendNotification(String ticker, String txt) {
+		Context ctx = this;
+		Intent notificationIntent = new Intent(ctx, TestActivity.class);
+		PendingIntent contentIntent = PendingIntent.getActivity(ctx,
+				PI_REQ_CODE, notificationIntent,
+				PendingIntent.FLAG_CANCEL_CURRENT);
+
+		NotificationManager nm = (NotificationManager) ctx
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		Resources res = ctx.getResources();
+		Notification.Builder builder = new Notification.Builder(ctx);
+
+		// res.getString(R.string.myTicker)
+		Bitmap bmp = BitmapFactory.decodeResource(res, R.drawable.ic_launcher);
+		builder.setContentIntent(contentIntent)
+				.setSmallIcon(R.drawable.ic_launcher).setLargeIcon(bmp)
+				.setTicker(ticker).setWhen(System.currentTimeMillis())
+				.setAutoCancel(true)
+				.setContentTitle(res.getString(R.string.myNotifTitle))
+				.setContentText(txt);
+		Notification n = builder.build();
+
+		nm.notify(MY_NOTIF_ID, n);
+	}
+}
